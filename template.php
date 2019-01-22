cccf6317610dfb880123dd3d59282064ed1273be
ChanV <chan.vancia@gmail.com>
2019-01-22 18:56:51 +0100

    Modification du template

--- template.php
+++ template.php
@@ -27,7 +27,7 @@
 						</div>
 					</div>
 				</div>
-				<div class="navbar fixed-top navbar-dark bg-dark shadow-sm">
+				<div class="navbar navbar-dark bg-dark shadow-sm">
 					<div class="container d-flex justify-content-between">
 						<a href="#" class="navbar-brand d-flex align-items-center">
 						<strong>Mon blog</strong>
