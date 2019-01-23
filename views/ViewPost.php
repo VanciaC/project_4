84885bf9703538ff46c8b8a2d39d2066c7ad7f1f
ChanV <chan.vancia@gmail.com>
2019-01-23 19:29:10 +0100

    Mise a jour de la vue d'un Post. Mise en page faite.

--- views/ViewPost.php
+++ views/ViewPost.php
@@ -1,35 +1,39 @@
 <?php $this->title = "Mon blog - " . $post['title']; ?>
 
-<article>
-	<header>
-		<h1 class="postTitle"><?= htmlspecialchars($post['title']); ?></h1>
+<article class="card text-center shadow">
+	<div class="card-body">
+		<h1 class="card-title text-info"><?= htmlspecialchars($post['title']); ?></h1>
+		<p class="card-text"><?= htmlspecialchars($post['content']); ?></p>
+	</div>
+	<div class="card-footer text-mutes">
 		<time><?= $post['date_creation']; ?></time>
-	</header>
-	<p><?= htmlspecialchars($post['content']); ?></p>
+	</div>
 </article>
 <hr/>
 
-<h2 class="comment">Commentaires</h2>
+<h5 class="text-center text-white">Commentaires</h5>
 <?php foreach ($comments as $comment): ?>
-
-  <p><?= htmlspecialchars($comment['author']); ?> dit :  <?= htmlspecialchars($comment['comment']) ?></p>
-  <p><?= $comment['date_comment']; ?></p>
-
+<div class="container">
+	<p class="text-light"><span class="font-weight-bold text-white"><?= htmlspecialchars($comment['author']); ?> :</span>
+		<br/><?= htmlspecialchars($comment['comment']); ?>
+	</p>
+	<hr/>
+</div>
 <?php endforeach; ?>
 <br/>
-
-<hr/>
-
-<h3>Ajouter un commentaire</h3>
-<form method="POST" action="index.php?action=comment">
-	<p><label for="author">Votre pseudo</label> : <input type="text" name="author" id="author" required /></p>
-	<p>
-		<label for="comment">
-		Votre commentaire :
-		</label>
-		<br />
-		<textarea name="comment" id="comment" rows="10" cols="50" required></textarea>
-	</p>
-	 <input type="hidden" name="idPost" value="<?= $post['id'] ?>" />
-	<p><input type="submit" value="Envoyer"/></p>  
-</form>
+<div class="container">
+	<h5 class="text-center text-white">Ajouter un commentaire</h5>
+<br/>
+	<form method="POST" action="index.php?action=comment">
+		<div class="form-group">
+			<label for="author" class="text-white">Votre pseudo</label><input type="text" class="form-control" name="author" id="author" required />
+		</div>
+		<div class="form-group">
+			<label for="comment" class="text-white">Votre commentaire</label>
+			<textarea class="form-control" name="comment" id="comment" rows="10" cols="50" required></textarea>
+		</div>
+		 <input type="hidden" name="idPost" value="<?= $post['id'] ?>" />
+		<button type="submit" class="btn btn-info">Envoyer</button>
+	</form>
+</div>
+<br/>
